50a1114fcf0894295c98613a28677d99a5bb0d10
Liviu Jianu <liviu.jianu@gmail.com>
2014-05-04 02:41:13 +0300

    Eliminare metoda nefolosita salvareDB din MySQL.java

--- src/database/MySQL.java
+++ src/database/MySQL.java
@@ -1,22 +1,26 @@
 package database;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class MySQL {
 
-    private static final String db = "agenda";
-    private static final String user = "agenda";
-    private static final String pass = "agenda1";
-    private static final String url = "jdbc:mysql://localhost:3306/" + db;
-    private static Connection Conn;
-
-    public static Connection getConnection() {
-        try {
-            Class.forName("com.mysql.jdbc.Driver");
-            Conn = DriverManager.getConnection(url, user, pass);
-        } catch (ClassNotFoundException | SQLException e) {
-        }
-        return Conn;
-    }
+	private static final String db="agenda";
+	private static final String user="agenda";
+	private static final String pass="agenda1";
+	private static final String url="jdbc:mysql://localhost:3306/"+db;
+	private static Connection Conn;
+	
+	
+	public static Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Conn = DriverManager.getConnection(url,user,pass);
+		} catch(ClassNotFoundException | SQLException e) {
+                    e.printStackTrace();
+		}
+		return Conn;
+	}
 }
