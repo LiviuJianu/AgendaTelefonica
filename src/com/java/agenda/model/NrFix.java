2807fe463d10f032ca8c0148c6b2040157918448
Liviu Jianu <liviu.jianu@gmail.com>
2016-09-27 23:00:14 +0300

    deleted comments
    delete comments from self explanatory methods ( get, set, constructors)

--- src/com/java/agenda/model/NrFix.java
+++ src/com/java/agenda/model/NrFix.java
@@ -1,47 +1,22 @@
 package com.java.agenda.model;
 
-/**
- * Obiect NumarFix extinde NumarTelefon
- *
- * @author Liviu Jianu
- */
 public class NrFix extends NrTelefon {
 
     private String fix;
 
-    /**
-     * Constructor
-     *
-     * @param fix sir de caractere care reprezinta numarul de telefon fix
-     */
     public NrFix(String fix) {
         super(fix);
         this.fix = fix;
     }
 
-    /**
-     * Getter
-     *
-     * @return fix Numar de telefon fix
-     */
     public String getFix() {
         return fix;
     }
 
-    /**
-     * Setter
-     *
-     * @param fix Modificare numar de telefon fix
-     */
     public void setFix(String fix) {
         this.fix = fix;
     }
 
-    /**
-     * Afisare formatata a numarului de telefon
-     *
-     * @return fix numarul de telefon fix
-     */
     @Override
     public String toString() {
         return fix;
--- src/com/java/agenda/model/NrTelefon.java
+++ src/com/java/agenda/model/NrTelefon.java
@@ -1,46 +1,21 @@
 package com.java.agenda.model;
 
-/**
- * Obiect NumarTelefon - clasa parinte pentru <code>NrFix</code> si <code>NrMobil</code>
- *
- * @author Liviu Jianu
- */
 public abstract class NrTelefon {
 
     private String numar;
 
-    /**
-     * Constructor
-     *
-     * @param numar sir de caractere care reprezinta numarul de telefon generic
-     */
     public NrTelefon(String numar) {
         this.numar = numar;
     }
 
-    /**
-     * Getter
-     *
-     * @return numar Numarul de telefon
-     */
     public String getNumar() {
         return numar;
     }
 
-    /**
-     * Setter
-     *
-     * @param numar Modificare numar de telefon
-     */
     public void setNumar(String numar) {
         this.numar = numar;
     }
 
-    /**
-     * Afisare formatata a numarului de telefon
-     *
-     * @return numar numarul de telefon
-     */
     @Override
     public String toString() {
         return numar;
