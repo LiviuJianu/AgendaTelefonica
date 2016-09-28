42e1e09de90af107bc097ed3c97cc71c882c5e9f
Liviu Jianu <liviu.jianu@gmail.com>
2016-09-29 00:30:29 +0300

    change the layout manager of the details panel PanouDetalii.java
    it now uses the GridBagLayout to arrange the components

--- src/com/java/agenda/view/PanouDetalii.java
+++ src/com/java/agenda/view/PanouDetalii.java
@@ -28,13 +28,35 @@ public class PanouDetalii extends JPanel {
     }
 
     private void init() {
-        setLayout(new GridLayout(3,3));
-        add(imagineLabel);
-        add(numeLabel);
-        add(prenumeLabel);
-        add(cnpLabel);
-        add(cnpText);
-        add(telefonLabel);
-        add(telefonText);
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        add(imagineLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 0;
+        add(numeLabel, gbc);
+
+        gbc.gridx = 2;
+        gbc.gridy = 0;
+        add(prenumeLabel, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        add(cnpLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 1;
+        add(cnpText, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        add(telefonLabel, gbc);
+
+        gbc.gridx = 1;
+        gbc.gridy = 2;
+        add(telefonText, gbc);
     }
 }
