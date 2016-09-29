19f0bb92d67464312dd009857db1463dda8ff2a1
Liviu Jianu <liviu.jianu@gmail.com>
2016-09-29 07:28:44 +0300

    reprganized the details panel to properly align and display the fields

--- src/com/java/agenda/view/PanouDetalii.java
+++ src/com/java/agenda/view/PanouDetalii.java
@@ -7,8 +7,8 @@ public class PanouDetalii extends JPanel {
 
     private JLabel imagineLabel;
     private ImageIcon icoanaImagine;
-    private JLabel numeLabel;
-    private JLabel prenumeLabel;
+    private JTextField numeText;
+    private JTextField prenumeText;
     private JLabel cnpLabel;
     private JTextField cnpText;
     private JLabel telefonLabel;
@@ -17,8 +17,8 @@ public class PanouDetalii extends JPanel {
     public PanouDetalii() {
         icoanaImagine = new ImageIcon("resources/icon/noImage.png");
         imagineLabel = new JLabel(icoanaImagine);
-        numeLabel = new JLabel("Nume");
-        prenumeLabel = new JLabel("Prenume");
+        numeText = new JTextField("Nume");
+        prenumeText = new JTextField("Prenume");
         cnpLabel = new JLabel("CNP: ");
         cnpText = new JTextField(13);
         telefonLabel = new JLabel("Telefon");
@@ -31,32 +31,41 @@ public class PanouDetalii extends JPanel {
         setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
 
+
         gbc.gridx = 0;
         gbc.gridy = 0;
+        gbc.ipadx = 0;
+        gbc.insets = new Insets(0,0,50,30);
         add(imagineLabel, gbc);
 
         gbc.gridx = 1;
         gbc.gridy = 0;
-        add(numeLabel, gbc);
+        gbc.ipadx = 100;
+        add(numeText, gbc);
 
         gbc.gridx = 2;
         gbc.gridy = 0;
-        add(prenumeLabel, gbc);
+        gbc.ipadx = 100;
+        add(prenumeText, gbc);
 
         gbc.gridx = 0;
         gbc.gridy = 1;
+        gbc.ipadx = 0;
         add(cnpLabel, gbc);
 
         gbc.gridx = 1;
         gbc.gridy = 1;
+        gbc.ipadx = 100;
         add(cnpText, gbc);
 
         gbc.gridx = 0;
         gbc.gridy = 2;
+        gbc.ipadx = 0;
         add(telefonLabel, gbc);
 
         gbc.gridx = 1;
         gbc.gridy = 2;
+        gbc.ipadx = 100;
         add(telefonText, gbc);
     }
 }
