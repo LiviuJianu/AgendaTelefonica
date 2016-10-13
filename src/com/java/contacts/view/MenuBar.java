70d68c274dd7766b71a17858031de6f043181979
Liviu Jianu <liviu.jianu@gmail.com>
2016-10-13 22:37:03 +0300

    enabled app registration and regorganized the code into smaller methods

--- src/com/java/contacts/view/MenuBar.java
+++ src/com/java/contacts/view/MenuBar.java
@@ -1,5 +1,7 @@
 package com.java.contacts.view;
 
+import com.java.contacts.view.listeners.ExitAppActionListener;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,103 +10,143 @@ import java.awt.event.KeyEvent;
 public class MenuBar extends JMenuBar {
 
     private final ImageIcon loadIcon = new ImageIcon("resources/icon/open.png");
-    private final ImageIcon saveIcon = new ImageIcon("resources/icon/save.png");
-    private final ImageIcon exitIcon = new ImageIcon("resources/icon/exit.png");
-    private final ImageIcon keyIcon = new ImageIcon("resources/icon/key.png");
-
     private final JMenuItem loadButton = new JMenuItem("Load DB", loadIcon);
+    private final ImageIcon saveIcon = new ImageIcon("resources/icon/save.png");
     private final JMenuItem saveButton = new JMenuItem("Save DB", saveIcon);
+    private final ImageIcon exitIcon = new ImageIcon("resources/icon/exit.png");
     private final JMenuItem exitButton = new JMenuItem("Exit", exitIcon);
+    private final ImageIcon keyIcon = new ImageIcon("resources/icon/key.png");
     private final JMenuItem registerButton = new JMenuItem("Register", keyIcon);
-    private ImageIcon newIcon = new ImageIcon("resources/icon/new.png");
-    private ImageIcon updateIcon = new ImageIcon("resources/icon/update.png");
-    private ImageIcon searchIcon = new ImageIcon("resources/icon/search.png");
-    private ImageIcon deleteIcon = new ImageIcon("resources/icon/trash.png");
-    private ImageIcon helpIcon = new ImageIcon("resources/icon/help.png");
-
-    JMenu file = new JMenu("File");
+    private final JMenuItem saveContact = new JMenuItem("Save", saveIcon);
+    private final JMenu file = new JMenu("File");
+    private final JMenu contactsMenu = new JMenu("Contacts");
+    private final ImageIcon newIcon = new ImageIcon("resources/icon/new.png");
+    private final JMenuItem newContact = new JMenuItem("Add", newIcon);
+    private final ImageIcon updateIcon = new ImageIcon("resources/icon/update.png");
+    private final JMenuItem updateContact = new JMenuItem("Update", updateIcon);
+    private final ImageIcon searchIcon = new ImageIcon("resources/icon/search.png");
+    private final JMenuItem searchContact = new JMenuItem("Search", searchIcon);
+    private final ImageIcon deleteIcon = new ImageIcon("resources/icon/trash.png");
+    private final JMenuItem deleteContact = new JMenuItem("Delete", deleteIcon);
+    private final JMenu helpMenu = new JMenu("Help");
+    private final ImageIcon helpIcon = new ImageIcon("resources/icon/help.png");
+    private final JMenuItem manual = new JMenuItem("Manual", helpIcon);
+    private boolean isAppRegistered = false;
 
     public MenuBar() {
         init();
     }
 
     public void init() {
-        file.setMnemonic(KeyEvent.VK_F);
+        setupFileMenu();
+        setupContactsMenu();
+        setupHelpMenu();
+    }
 
+    private void setupFileMenu() {
+        file.setMnemonic(KeyEvent.VK_F);
+        setupLoadButton();
+        setupSaveButton();
+        setupExitButton();
+        file.add(loadButton);
+        file.add(saveButton);
+        file.addSeparator();
+        file.add(exitButton);
+
+        this.add(file);
+    }
+
+    private void setupContactsMenu() {
+        contactsMenu.setMnemonic(KeyEvent.VK_B);
+        setupNewContactMenu();
+        setupUpdateContactMenu();
+        setupSearchContactMenu();
+        setupSaveContactMenu();
+        setupDeleteContactMenu();
+        contactsMenu.add(newContact);
+        contactsMenu.add(updateContact);
+        contactsMenu.add(saveContact);
+        contactsMenu.add(deleteContact);
+        contactsMenu.addSeparator();
+        contactsMenu.add(searchContact);
+
+        this.add(contactsMenu);
+    }
+
+    private void setupHelpMenu() {
+        helpMenu.setMnemonic(KeyEvent.VK_H);
+        setupManualMenu();
+        setupRegisterMenu();
+        helpMenu.add(registerButton);
+        helpMenu.addSeparator();
+        helpMenu.add(manual);
+
+        this.add(helpMenu);
+    }
+
+    private void setupLoadButton() {
         loadButton.setMnemonic(KeyEvent.VK_D);
         loadButton.setToolTipText("Load Database");
+        loadButton.setEnabled(isAppRegistered);
 //        loadButton.addActionListener(new IncarcaDB());
+    }
 
+    private void setupSaveButton() {
         saveButton.setMnemonic(KeyEvent.VK_A);
         saveButton.setToolTipText("Save database");
+        saveButton.setEnabled(isAppRegistered);
 //        saveButton.addActionListener(new SalveazaDB());
+    }
 
-        /*
-         * Cand se doreste iesirea din aplicatie, utilizatorul este intrebat
-         * daca se doreste iesirea, caz in care aplicatia se inchide, sau daca
-         * nu se doreste iesire, aplicatia functioneaza in continuare.
-         */
+    private void setupExitButton() {
         exitButton.setMnemonic(KeyEvent.VK_I);
         exitButton.setToolTipText("Exit the app");
         exitButton.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, ActionEvent.CTRL_MASK));
-        exitButton.addActionListener(new ActionListener() {
+        exitButton.addActionListener(new ExitAppActionListener());
+    }
 
-            @Override
-            public void actionPerformed(ActionEvent ae) {
-                int dialogButton = JOptionPane.YES_NO_OPTION;
-                int exitDialogResult = JOptionPane.showConfirmDialog(null, "Confirm exiting the app?", "Exit application", dialogButton);
-                if (exitDialogResult == JOptionPane.YES_OPTION) {
-                    System.exit(0);
-                }
-            }
-        });
-
-        JMenu contactsMenu = new JMenu("Contacts");
-        contactsMenu.setMnemonic(KeyEvent.VK_B);
-
-        JMenuItem newContact = new JMenuItem("Add", newIcon);
+    private void setupNewContactMenu() {
         newContact.setMnemonic(KeyEvent.VK_A);
         newContact.setToolTipText("Add new contact");
         newContact.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_A, ActionEvent.CTRL_MASK));
 //        abonatNou.addActionListener(new AbonatPopup());
+    }
 
-        JMenuItem updateContact = new JMenuItem("Update", updateIcon);
+    private void setupUpdateContactMenu() {
         updateContact.setMnemonic(KeyEvent.VK_U);
         updateContact.setToolTipText("Update contact");
         updateContact.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_U, ActionEvent.CTRL_MASK));
 //        updateContact.addActionListener(new AbonatPopup());
+    }
 
-        JMenuItem searchContact = new JMenuItem("Search", searchIcon);
+    private void setupSearchContactMenu() {
         searchContact.setMnemonic(KeyEvent.VK_C);
         searchContact.setToolTipText("Search contact");
         searchContact.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_C, ActionEvent.CTRL_MASK));
 //        searchContact.addActionListener(new CautaAbonat());
+    }
 
-        JMenuItem saveContact = new JMenuItem("Save", saveIcon);
+    private void setupSaveContactMenu() {
         saveContact.setMnemonic(KeyEvent.VK_S);
         saveContact.setToolTipText("Save contact");
         saveContact.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK));
 //        saveContact.addActionListener(new AdaugaAbonat());
+    }
 
-        JMenuItem deleteContact = new JMenuItem("Delete", deleteIcon);
+    private void setupDeleteContactMenu() {
         deleteContact.setMnemonic(KeyEvent.VK_R);
         deleteContact.setToolTipText("Delete contact");
         deleteContact.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_R, ActionEvent.CTRL_MASK));
 //        deleteContactFromView.addActionListener(new StergeAbonat());
+    }
 
-        JMenu helpMenu = new JMenu("Help");
-        helpMenu.setMnemonic(KeyEvent.VK_H);
-
-        JMenuItem manual = new JMenuItem("Manual", helpIcon);
+    private void setupManualMenu() {
         manual.setMnemonic(KeyEvent.VK_M);
         manual.setToolTipText("Users guide");
         manual.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_M, ActionEvent.CTRL_MASK));
+    }
 
-        /*
-         * Pentru inregistrarea aplicatiei, utilizatorul trebuie sa intriduca
-         * un cod de inregistrare, care din motive de usurinta de testare
-         * este urmatorul : 123abc
-         */
+    private void setupRegisterMenu() {
         registerButton.setMnemonic(KeyEvent.VK_R);
         registerButton.setToolTipText("Enter registration code");
         registerButton.addActionListener(new ActionListener() {
@@ -113,36 +155,18 @@ public class MenuBar extends JMenuBar {
             public void actionPerformed(ActionEvent ae) {
                 String serialNumber = JOptionPane.showInputDialog("Registration code");
                 if (serialNumber != null) {
-//                    if (isAppRegistered(serialNumber)) {
-//                        activeazaAplicatie();
-//                        JOptionPane.showMessageDialog(null, "Aplicatia a fost inregistrata!");
-//                    } else {
-//                        JOptionPane.showMessageDialog(null, "Cod de inregistrare incorect!");
-//                    }
+                    if (serialNumber.equals(123)) {
+                        isAppRegistered = true;
+                        loadButton.setEnabled(isAppRegistered);
+                        saveButton.setEnabled(isAppRegistered);
+                        registerButton.setEnabled(!isAppRegistered);
+                        JOptionPane.showMessageDialog(null, "Aplicatia a fost inregistrata!");
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Cod de inregistrare incorect!");
+                    }
                 }
             }
         });
-
-        file.add(loadButton);
-        file.add(saveButton);
-        file.addSeparator();
-        file.add(exitButton);
-
-        contactsMenu.add(newContact);
-        contactsMenu.add(updateContact);
-        contactsMenu.add(saveContact);
-        contactsMenu.add(deleteContact);
-        contactsMenu.addSeparator();
-        contactsMenu.add(searchContact);
-
-        helpMenu.add(registerButton);
-        helpMenu.addSeparator();
-        helpMenu.add(manual);
-
-        this.add(file);
-        this.add(contactsMenu);
-        this.add(helpMenu);
-
     }
 
 }
