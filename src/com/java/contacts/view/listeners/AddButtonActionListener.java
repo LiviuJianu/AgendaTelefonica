40d86103c9f23d9fafe12c811915200434a879d0
Liviu Jianu <liviu.jianu@gmail.com>
2016-10-13 22:47:49 +0300

    add new ActionListener to be used for Add button

---
+++ src/com/java/contacts/view/listeners/AddButtonActionListener.java
@@ -0,0 +1,11 @@
+package com.java.contacts.view.listeners;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AddButtonActionListener implements ActionListener {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
